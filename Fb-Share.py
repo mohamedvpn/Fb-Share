11f5cd8f2e0395ce51959c8b9bf3e18d8bd3658e
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-10-28 23:11:45 +0300

    Update Fb-Share.py

--- Fb-Share.py
+++ Fb-Share.py
@@ -8,5 +8,6 @@
   "IJAF-S3Y1-5574-HDET":{'name':'Zain Abdelrahman','end_date':'11/16/2024'},
   "IJAF-S3Y1-5574-5687":{'name':'Deyaa Safwat','end_date':'11/16/2024'},
   "IJAF-4767-5574-JDYJ":{'name':'Mustafa Omar','end_date':'11/16/2024'},
-  "IJAF-IJAF-IJAF-JDYJ":{'name':'امجد سامي','end_date':'11/16/2024'}
+  "IJAF-IJAF-IJAF-JDYJ":{'name':'امجد سامي','end_date':'11/16/2024'},
+  "FDGR-IJAF-IJAF-DB43":{'name':'M Y. Hassan','end_date':'11/16/2024'}
 }
