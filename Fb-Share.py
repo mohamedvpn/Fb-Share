0ba334ae6f455d7e3e1d0cfd925477f804dce73a
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-10-28 19:45:41 +0300

    Update Fb-Share.py

--- Fb-Share.py
+++ Fb-Share.py
@@ -3,5 +3,6 @@
   "ZP59-6KKX-DCBK-O2RN":{'name':'Ahmed Samir','end_date':'11/16/2024'},
   "ZP59-6KKX-DCBK-0HEX":{'name':'Tarkhawy','end_date':'11/16/2024'},
   "IJAF-S3Y1-86FV-QBH2":{'name':'Ahmed Ali','end_date':'11/16/2024'},
-  "IJAF-S3Y1-86FV-1221":{'name':'Mahmoud Hamza','end_date':'11/16/2024'}
+  "IJAF-S3Y1-86FV-1221":{'name':'Mahmoud Hamza','end_date':'11/16/2024'},
+  "IJAF-S3Y1-5644-1221":{'name':'Ahmed A. Elsamalosy','end_date':'11/16/2024'}
 }
