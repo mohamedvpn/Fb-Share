366d66cb12a5b0cc367376ea36536d994c4a971b
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-10-28 18:08:47 +0300

    Update Fb-Share.py

--- Fb-Share.py
+++ Fb-Share.py
@@ -1,5 +1,5 @@
 {
   "6JJ8-DGGR-3ZD4-YTPL":{'name':'Eslam','end_date':'11/16/2024'},
-  "2246546546":{'name':'Wesam','end_date':'11/16/2024'},
+  "ZP59-6KKX-DCBK-O2RN":{'name':'Ahmed Samir','end_date':'11/16/2024'},
   
 }
