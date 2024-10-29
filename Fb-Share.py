b470057a9cd01eb1da0a78ccc30ad14f6aefa7cf
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-10-29 23:13:58 +0300

    Update Fb-Share.py

--- Fb-Share.py
+++ Fb-Share.py
@@ -27,7 +27,7 @@
   "5767-GSHR-6464-4444":{'name':'Mohamed Ahmed','end_date':'11/16/2024'},
 
   
-  "4678-GSHR-6464-8689":{'name':'','end_date':'11/16/2024'},
-  "08k9-GSHR-6464-757s":{'name':'','end_date':'11/16/2024'}
+  "4678-GSHR-6464-8689":{'name':'Mohamed El Nagar','end_date':'11/16/2024'},
+  "08k9-GSHR-6464-757s":{'name':'Zoro Ahmed','end_date':'11/16/2024'}
   
 }
